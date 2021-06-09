5f97decf057573c44b69a200d40c0a6275ff1b59
Yuchan Iizuka <yuchan4869@gmx.com>
2021-06-09 19:57:13 +0200

    config for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./models/config");
 
@@ -10,15 +11,26 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "API running..." });
-});
+
 
 //routes
 app.use("/api/availability", require("./routes/dateRoutes"));
 app.use("/api/save", require("./routes/saveRoutes"));
 app.use("/api/check", require("./routes/checkRoutes"));
 app.use("/api/delete", require("./routes/deleteRoutes"));
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client", "build", "index.html"))
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.json({ message: "API running..." });
+    });
+}
+
 
 const PORT = process.env.PORT || 5000;
 
